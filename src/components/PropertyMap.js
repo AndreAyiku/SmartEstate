a3c524d246c9836816edd41ce1bed8d90138b7ac
AndreAyiku <andre.ayiku@ashesi.edu.gh>
2025-05-03 10:51:42 +0000

    .

--- src/components/PropertyMap.js
+++ src/components/PropertyMap.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import styles from '../styles/Map.Module.css';
+import styles from '../styles/Map.module.css';
 
 const PropertyMap = ({ property }) => {
   const mapRef = useRef(null);
